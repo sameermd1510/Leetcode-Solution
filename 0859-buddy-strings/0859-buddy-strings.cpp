0f2afefe86a8462346a7756f06fdc617713dc7be
Mohammad Sameer <53491807+sameermd1510@users.noreply.github.com>
2023-07-03 18:27:28 +0530

    Time: 6 ms (44.19%), Space: 6.9 MB (52.51%) - LeetHub

--- 0859-buddy-strings/0859-buddy-strings.cpp
+++ 0859-buddy-strings/0859-buddy-strings.cpp
@@ -8,28 +8,22 @@ public:
         int f[26]={};
         
         for( char ch:s) {
-            f[ch-'a']++;
-            if(f[ch-'a']>1) c=1;
+            if(++f[ch-'a']>1) c=1;
         }
-        
         if(s==g ) return c;
+        
         for( ;i<s.size();i++){
-            if(s[i]==g[j]){
-                j++;
-            }
+            if(s[i]==g[j]) j++;
             else break; 
         }
         int k=i;
         i++;
         j++;
         for( ;i<g.size();i++){
-            if(s[i]==g[j]){
-                j++;
-            }
+            if(s[i]==g[j]) j++;
             else break;
         }
         swap(s[k],s[i]);
-        cout<<s<<"\n";
         return s==g ;
     }
 };
