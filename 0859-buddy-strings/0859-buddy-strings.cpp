31123ef2ea14164a622ae31443f7b2cac708a7f4
Mohammad Sameer <53491807+sameermd1510@users.noreply.github.com>
2023-07-03 18:32:43 +0530

    Time: 6 ms (44.19%), Space: 6.9 MB (52.51%) - LeetHub

--- 0859-buddy-strings/0859-buddy-strings.cpp
+++ 0859-buddy-strings/0859-buddy-strings.cpp
@@ -1,18 +1,24 @@
 class Solution {
 public:
     bool buddyStrings(string s, string g) {
-        if(s.size()!=g.size() ) return 0;
+        if(s.size()!=g.size() ) return 0; // if not equal length
         
         int i=0,j=0,c=0;
         
         int f[26]={};
         
-        for( char ch:s) {
-            if(++f[ch-'a']>1) c=1;
+        for( char ch:s) {  // if frequency of any charater is greater than 1  
+            if(++f[ch-'a']>1){ 
+                c=1;break;
+            }                 
         }
-        if(s==g ) return c;
+        if(s==g ) return c;  // if string is already destination string then it depend on c, if frequency of any charachter is greater than 1
         
-        for( ;i<s.size();i++){
+        
+        /*
+        finding two index where  character mismatched.
+        */
+        for( ;i<s.size();i++){    
             if(s[i]==g[j]) j++;
             else break; 
         }
@@ -23,7 +29,7 @@ public:
             if(s[i]==g[j]) j++;
             else break;
         }
-        swap(s[k],s[i]);
+        swap(s[k],s[i]);  // swap it.
         return s==g ;
     }
 };
