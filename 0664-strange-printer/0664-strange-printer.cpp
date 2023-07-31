93dddc063f1ba46fd3a89ca9b274b2e8542983b8
Mohammad Sameer <53491807+sameermd1510@users.noreply.github.com>
2023-07-31 13:50:45 +0530

    Time: 121 ms (32.34%), Space: 9.5 MB (26.92%) - LeetHub

---
+++ 0664-strange-printer/0664-strange-printer.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int strangePrinter(string s) {
+        //s.erase(unique(s.begin(),s.end()),s.end());
+        
+        int n=s.size();
+        vector< vector< int>> dp( n, vector< int> (n,n));
+        
+        for( int i=0;i<n;i++){
+            dp[i][i]=1;
+            
+            if(i+1<n ) dp[i][i+1]=2-(s[i]==s[i+1]);
+        }
+        
+        for( int l=2;l<=n;l++){
+            for( int st=0;st+l<n;st++){
+                int e=l+st;
+                //dp[st][e]=l+1;
+                for( int k=st;k<e;k++){
+                    dp[st][e]=min( dp[st][e]  ,  dp[st][k]+dp[k+1][e]-(s[st]==s[e]));
+                }
+            }
+        }
+        return dp[0][n-1];
+        
+    }
+};
