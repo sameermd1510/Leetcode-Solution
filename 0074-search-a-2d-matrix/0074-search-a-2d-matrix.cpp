18ea250802935a116347931f1c5ec42dbcf758f3
Mohammad Sameer <53491807+sameermd1510@users.noreply.github.com>
2023-08-08 04:10:02 +0530

    Time: 3 ms (82.65%), Space: 9.5 MB (52.97%) - LeetHub

---
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& m, int t) 
+    {
+        int r=m.size(),c=m[0].size();
+        for(int i=0;i<r;i++)
+        {
+            if(m[i][0] <=t && m[i][c-1]>=t)
+            {
+                auto it=lower_bound(m[i].begin(),m[i].end(),t);
+                if(it!=m[i].end() && *it==t) return true;
+                else return false;
+            }
+        }
+        return false;
+    }
+};
