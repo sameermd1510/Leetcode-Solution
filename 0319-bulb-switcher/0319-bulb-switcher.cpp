70554cc10cd5ee356faa3d357fc768045c2b4346
Mohammad Sameer <53491807+sameermd1510@users.noreply.github.com>
2023-12-01 21:22:50 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (35.65%) - LeetHub

---
+++ 0319-bulb-switcher/0319-bulb-switcher.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    // n=5
+    // 0-[ 0 0 0 0 0 ]
+    // 1-[ 1 1 1 1 1 ]
+    // 2-[ 1 0 1 0 1 ]
+    // 3-[ 1 0 0 0 0 ]
+    // 4-[ 1 0 0 1 0 ]
+    // 5-[ 1 0 0 1 1 ]
+    int bulbSwitch(int n) {
+        
+        return (int)sqrt(n);
+        
+    }
+};
