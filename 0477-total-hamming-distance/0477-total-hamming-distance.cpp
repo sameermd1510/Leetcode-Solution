9a79c27ee04cd2fc184870e1894e6fa903b22538
Mohammad Sameer <53491807+sameermd1510@users.noreply.github.com>
2023-12-01 20:54:58 +0530

    Time: 34 ms (82.75%), Space: 19.4 MB (51.94%) - LeetHub

--- 0477-total-hamming-distance/0477-total-hamming-distance.cpp
+++ 0477-total-hamming-distance/0477-total-hamming-distance.cpp
@@ -1,14 +1,32 @@
 class Solution {
 public:
-//     2 4 14    
-//     2 -0010
-//     4 -0100
-//     6 -0110
-//     14-1110
-      
-//     z -3114
-//     on-1330
-//       -3330
+    int totalHammingDistance(vector<int>& a) {
+        int n=a.size(),ans=0;
+        vector< int> on( 32, 0);
+        
+        for( int i=0;i<32;i++){
+            for( int x:a){
+                on[i]+=x>>i & 1;
+            }
+            ans+=on[i]*(n-on[i]);
+        }
+        return ans;
+        // for( int x:a){
+        //     for( int v=x ,in=0 ; v!=0 ; v=v/2 , in++ ){
+        //         if(v%2) on[in]++;
+        //     }
+        // }
+        // int ans=0;
+        // for( int i=0;i<32;i++){
+        //     ans+=on[i]*(n-on[i]);
+        // }
+        // return ans;
+    }
+};
+/*
+when we are separatly calculating the umber of one for each number for each bits.
+class Solution {
+public:
     int totalHammingDistance(vector<int>& a) {
         int n=a.size();
         vector< int> on( 32, 0);
@@ -25,3 +43,4 @@ public:
         return ans;
     }
 };
+*/
