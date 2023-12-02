572e547113474f5f1732ea7b73759984fce760c9
Mohammad Sameer <53491807+sameermd1510@users.noreply.github.com>
2023-12-02 19:22:31 +0530

    Time: 309 ms (75.91%), Space: 31.9 MB (76.55%) - LeetHub

--- 2915-length-of-the-longest-subsequence-that-sums-to-target/2915-length-of-the-longest-subsequence-that-sums-to-target.cpp
+++ 2915-length-of-the-longest-subsequence-that-sums-to-target/2915-length-of-the-longest-subsequence-that-sums-to-target.cpp
@@ -1,20 +1,19 @@
 class Solution {
 public:
     int dp[1000+1][1000+1];
-    int sol( vector< int> &a, int i, int sm ,int l, int s){
+    int sol( vector< int> &a, int i, int sm ,int s){
+        
         if( s==sm) return 0;
-        
         if(i >=a.size() || s > sm ) return INT_MIN;
-        
         if(dp[i][s]!=-1) return dp[i][s]; 
         
-        return dp[i][s]=max(1+sol(a,i+1,sm,l+1,s+a[i]) , sol(a,i+1,sm,l,s) );
+        return dp[i][s]=max(1+sol(a,i+1,sm,s+a[i]) , sol(a,i+1,sm,s) );
         
     }
     int lengthOfLongestSubsequence(vector<int>& a, int sm) {
         
         memset(dp,-1,sizeof(dp));
-        int ans=sol(a,0,sm,0,0);
+        int ans=sol(a,0,sm,0);
         
         return ans > 0 ? ans :-1;
     }
