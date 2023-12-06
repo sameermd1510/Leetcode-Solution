fc531fc390425f79e848cbb054660d13952dbbc3
Mohammad Sameer <53491807+sameermd1510@users.noreply.github.com>
2023-12-06 15:16:39 +0530

    Time: 129 ms (31.96%), Space: 68 MB (33.97%) - LeetHub

--- 1679-max-number-of-k-sum-pairs/1679-max-number-of-k-sum-pairs.cpp
+++ 1679-max-number-of-k-sum-pairs/1679-max-number-of-k-sum-pairs.cpp
@@ -14,20 +14,13 @@ public:
             if(m.find(k-a[i])!=m.end() && m[k-a[i]] >0 ){
                 
                 if(k==2*a[i] && m[a[i]] <2) continue;
-                
-                
-                // cout<<a[i] <<" "<<"\n";
                 m[a[i]]--;
                 m[k-a[i]]--;
                 
                 if(m[a[i]]==0) m.erase(a[i]);
                 
-                if(m[k-a[i]]==0) m.erase(k-a[i]);
+                if( 2*a[i]!=k && m[k-a[i]]==0) m.erase(k-a[i]);
                 
-                // for ( auto t:m){
-                //     for( int i=1;i<=t.second;i++) cout<<t.first<<" ";
-                // }
-                // cout<<"\n";
                 ans++;
             }
         }
