43b9f818ed8e9b1c141eaa44e4a6d1ecfc1bbbe7
Mohammad Sameer <53491807+sameermd1510@users.noreply.github.com>
2023-12-16 05:55:23 +0530

    Time: 3 ms (97.78%), Space: 7.6 MB (98.01%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -5,14 +5,14 @@ public:
         if(s.size()!=t.size()) return 0;
         if(s==t) return 1;
         
-        int m=0 ,n=0 , sm=0,tm=0;
+        int m=0,sm=0;
         for( int i=0;i<s.size();i++){
             m=m ^ (1<<(s[i]-'a'));
-            n=n ^ ( 1<<(t[i]-'a'));
+            m=m ^ ( 1<<(t[i]-'a'));
             
             sm+=s[i]*s[i];
-            tm+=t[i]*t[i];
+            sm-=t[i]*t[i];
         }
-        return (m==n && sm==tm);
+        return (!m && !sm);
     }
 };
