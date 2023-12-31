d1d1f6b20f7305c2dafb6e2385368f160fe73d29
Mohammad Sameer <53491807+sameermd1510@users.noreply.github.com>
2023-12-31 14:03:12 +0530

    Time: 9 ms (6.77%), Space: 41.5 MB (8.77%) - LeetHub

--- 1624-largest-substring-between-two-equal-characters/1624-largest-substring-between-two-equal-characters.java
+++ 1624-largest-substring-between-two-equal-characters/1624-largest-substring-between-two-equal-characters.java
@@ -3,20 +3,15 @@ class Solution {
         
         HashMap< Character, Integer> m=new HashMap<>();
         int ans=0 , n=s.length();
-        int[] f=new int[26];
-        boolean fl=false;
         
         for( int i=0;i<n;i++){
             
             char l=s.charAt(i);
             char r=s.charAt(n-i-1);
-            
-            f[l-'a']++;
-            f[r-'a']++;
-            
+        
             if( m.containsKey(l)){
                 
-                ans=Math.max( ans , Math.abs(m.get(l)-i)-1);
+                ans=Math.max( ans , Math.abs(m.get(l)-i));
                 
                 m.put(l, Math.min(i, m.get(l)));
             }
@@ -25,7 +20,7 @@ class Solution {
             }
             
             if(m.containsKey(r)){
-                ans=Math.max( ans , Math.abs(m.get(r)-(n-i-1))-1);
+                ans=Math.max( ans , Math.abs(m.get(r)-(n-i-1)));
                 
                  m.put(r, Math.max(n-i-1, m.get(r)));
             }
@@ -33,14 +28,7 @@ class Solution {
                 m.put(r,(n-i-1));
             }
         }
-        for( int i=0;i<26;i++){
-            if( f[i]>2){
-                fl=true;
-                break;
-            }
-        }
-        if( !fl) return -1;
-        return ans<0 ? 0:ans;
+        return ans-1;
         
     }
 }
